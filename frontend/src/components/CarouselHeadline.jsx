f354f4b9ad33574583f42d19cf7c2e319a4cf4b0
arfcrrr <ajengarifa.char@gmail.com>
2022-06-29 17:54:53 +0700

    update image width in Carousel component to full width

--- frontend/src/components/CarouselHeadline.jsx
+++ frontend/src/components/CarouselHeadline.jsx
@@ -1,28 +1,34 @@
 import { Carousel } from 'antd';
+
 const contentStyle = {
     height: '500px',
+    width: '100%',
+    objectFit: 'cover',
     color: '#fff',
     lineHeight: '250px',
     textAlign: 'center',
     background: '#364d79',
 };
 
-const CarouselHeadline = ({src}) => (
+const CarouselHeadline = ({ src }) => (
     <Carousel autoplay>
         <div>
-            <h3 style={contentStyle}>
-                <img src={src}/>
-            </h3>
+            <img
+                style={contentStyle}
+                src="https://images.unsplash.com/photo-1498598457418-36ef20772bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            />
         </div>
         <div>
-            <h3 style={contentStyle}>
-                <img src={src}/>
-            </h3>
+            <img
+                style={contentStyle}
+                src="https://images.unsplash.com/photo-1463725876303-ff840e2aa8d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+            />
         </div>
         <div>
-            <h3 style={contentStyle}>
-                <img src={src}/>
-            </h3>
+            <img
+                style={contentStyle}
+                src="https://images.unsplash.com/photo-1500926165259-30ef3079d477?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            />
         </div>
     </Carousel>
 );
